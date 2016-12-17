5204808a9e2980b42a994c8b19362892ebf79c44
Zak Berkowitz <zberko1@tigers.lsu.edu>
2016-12-16 20:13:35 -0600

    Add PUN
    Add PUN functionality, in new scene “world-multiplayer.”  Include PUN
    basic demo for reference (should be deleted later), use PUN 3rd person
    character because it’s easier.  customize several  PUN demo scrips
    (added VKV suffix), as well as car prefab and spawn script to add
    network functionality.

---
+++ Assets/Scripts/AgentSpawner_VKV.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace com.kreweofvaporwave.parade
+{
+	public class AgentSpawner_VKV : MonoBehaviour {
+
+	    public GameObject m_agentPrefab;
+	    public int m_amountToSpawn;
+	    protected WaypointManager m_waypointManager;
+	    public float spawnInterval = 1.25f;
+	    protected int m_spawned = 0;
+	    public Transform m_spawnPoint;
+	    public bool m_infinite = false;
+
+		// Use this for initialization
+		void Start () {
+	        m_waypointManager = GetComponent<WaypointManager>();
+			if (PhotonNetwork.isMasterClient){
+	        	StartCoroutine(Spawn());
+			}
+			else {
+				Debug.Log("not master");
+			}
+		}
+
+	    IEnumerator Spawn()
+	    {
+	        yield return new WaitForSeconds(spawnInterval);
+
+			m_waypointManager.AddEntity((GameObject)PhotonNetwork.Instantiate(m_agentPrefab.name, m_spawnPoint.position, Quaternion.identity, 0));
+
+	        if (m_spawned++ < m_amountToSpawn - 1 | m_infinite)
+	            StartCoroutine(Spawn());
+	    }
+
+	}
+}
