fa1d9c8bef7c8630d339bd362bc61d9fc120f0fe
just-cady <cady@notnatural.co>
2017-02-19 18:34:17 -0600

    Multi float parade scripting process done

--- Assets/Scripts/ParadeManager.cs
+++ Assets/Scripts/ParadeManager.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 namespace com.kreweofvaporwave.parade
 {
 	public class ParadeManager : Photon.MonoBehaviour {
-		public string[] floats;
-		public int numberOfFloats = 1;
+		public GameObject[] floats;
+		public float SecondsToWait = 120.0f;
+		//public int numberOfFloats = 1;
 		//public float spawnInterval = 10.0f;
 		public Transform m_spawnPoint;
+		private int count = 0;
 
 		// Use this for initialization
 		void Start () {
 			m_spawnPoint = GameObject.Find("spawn point").transform;
-			floats = new string[numberOfFloats];
-			floats[0] = "Float1";
+			//floats = new string[numberOfFloats];
+			//floats[0] = "Float1";
 
 			//GetComponent<PhotonView>().RPC("RPCSpawn", PhotonTargets.All, numberOfFloats, spawnInterval);
 			StartCoroutine("Spawn");
@@ -25,9 +27,10 @@ namespace com.kreweofvaporwave.parade
 		{
 			while (true){
 				Debug.Log("before");
-				PhotonNetwork.InstantiateSceneObject(floats[0], m_spawnPoint.position, Quaternion.identity, 0, new object[0]);
-				yield return new WaitForSeconds(60.0f);
+				PhotonNetwork.InstantiateSceneObject(floats[count].name, m_spawnPoint.position, Quaternion.identity, 0, new object[0]);
+				yield return new WaitForSeconds(SecondsToWait);
 				Debug.Log("after");
+				count = (count + 1) % floats.Length;
 					/*floatCount++;
 
 					if (floatCount == numberOfFloats){
