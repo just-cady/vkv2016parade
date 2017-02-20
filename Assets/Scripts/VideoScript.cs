649e5ee0acdfbde687921d8e5ddc8c308def929c
just-cady <cady@notnatural.co>
2017-02-20 02:07:04 -0600

    Changed media to be local

--- Assets/Scripts/VideoScript.cs
+++ Assets/Scripts/VideoScript.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VideoScript : MonoBehaviour {
-	public string vidurl = "http://zakberkowitz.com/videos/vkvtestvid.ogv";
-	public string audiourl = "http://zakberkowitz.com/sounds/vkvtestaudio.ogg";
+	//public string vidurl = "http://zakberkowitz.com/videos/vkvtestvid.ogv";
+	//public string audiourl = "http://zakberkowitz.com/sounds/vkvtestaudio.ogg";
 	//WWW vidwww;
 	//WWW audiowww;
 	// Use this for initialization
-	void Start () {
-		StartCoroutine(PlayMovie(vidurl, audiourl));
 
+	void Start () {
+		//StartCoroutine(PlayMovie(vidurl, audiourl));
+		//((MovieTexture)GetComponent<Renderer> ().material.mainTexture).Play ();
+
+
+
+		MovieTexture movie = ((MovieTexture)GetComponent<Renderer> ().material.mainTexture);
+
+		movie.loop = true;
+		//yield return new WaitUntil(() => movie.isReadyToPlay);
+		AudioSource audSource = GetComponent<AudioSource>();
+		audSource.loop = true;
+		movie.Play();
+		audSource.Play();
 	}
 	
 	// Update is called once per frame
@@ -18,32 +30,33 @@ public class VideoScript : MonoBehaviour {
 		
 	}
 
-	IEnumerator PlayMovie(string vid, string aud){
-		WWW vidwww = new WWW(vid);
-		WWW audiowww = new WWW(aud);
-		MovieTexture movie = vidwww.movie;
-
-		movie.loop = true;
-		yield return new WaitUntil(() => movie.isReadyToPlay);
-
-		Renderer rend = GetComponent<Renderer>();
-		rend.material.mainTexture = movie;
-
-		AudioClip audClip = audiowww.GetAudioClip(true, true, AudioType.OGGVORBIS);
-
-		Debug.Log(audClip.loadState);
-
-		yield return new WaitUntil(() => audClip.loadState == AudioDataLoadState.Loaded);
-
-		Debug.Log(audClip.loadState);
-
-		AudioSource audSource = GetComponent<AudioSource>();
-		audSource.clip = audClip;
-		audSource.loop = true;
-
-
-		movie.Play();
-		audSource.Play();
-	}
+//	IEnumerator PlayMovie(string vid, string aud){
+//		//WWW vidwww = new WWW(vid);
+//		//WWW audiowww = new WWW(aud);
+//		//MovieTexture movie = vidwww.movie;
+//		MovieTexture movie = GetComponent<Renderer>().material.mainTexture;
+//	
+//		movie.loop = true;
+//		yield return new WaitUntil(() => movie.isReadyToPlay);
+//
+//		//Renderer rend = GetComponent<Renderer>();
+//		//rend.material.mainTexture = movie;
+//
+//		//AudioClip audClip = audiowww.GetAudioClip(true, true, AudioType.OGGVORBIS);
+//
+//		//Debug.Log(audClip.loadState);
+//
+//		//yield return new WaitUntil(() => audClip.loadState == AudioDataLoadState.Loaded);
+//
+//		//Debug.Log(audClip.loadState);
+//
+//		AudioSource audSource = GetComponent<AudioSource>();
+//		//audSource.clip = audClip;
+//		audSource.loop = true;
+//
+//
+//		movie.Play();
+//		audSource.Play();
+//	}
 					
 }
